e56b12076c01b8ca730184e6a24d2aa6cb09e608
Lennon Cook <lvc@codetripping.net>
2021-12-02 16:28:43 +1100

    day 2

---
+++ day_02.py
@@ -0,0 +1,57 @@
+
+
+
+def part_1(data):
+    r"""
+    >>> part_1('''\
+    ... forward 5
+    ... down 5
+    ... forward 8
+    ... up 3
+    ... down 8
+    ... forward 2''')
+    150
+
+    """
+    horiz, depth = (0, 0)
+
+    for line in data.splitlines():
+        direction, amount = line.strip().split()
+        amount = int(amount)
+
+        if direction == "forward":
+            horiz += amount
+        elif direction == "down":
+            depth += amount
+        elif direction == "up":
+            depth -= amount
+    return horiz * depth
+
+def part_2(data):
+    r"""
+    >>> part_2('''\
+    ... forward 5
+    ... down 5
+    ... forward 8
+    ... up 3
+    ... down 8
+    ... forward 2''')
+    900
+
+    """
+
+    horiz, depth, aim = (0, 0, 0)
+
+    for line in data.splitlines():
+        direction, amount = line.strip().split()
+        amount = int(amount)
+
+        if direction == "forward":
+            horiz += amount
+            depth += aim * amount
+        elif direction == "down":
+            aim += amount
+        elif direction == "up":
+            aim -= amount
+
+    return horiz * depth
