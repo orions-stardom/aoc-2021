cc4826aa17f660253d1a9a0b93fc6f9254ad16cb
Lennon Cook <lvc@codetripping.net>
2021-12-20 21:24:31 +1100

    maybe slowly working day 19 part 1

--- day_19.py
+++ day_19.py
@@ -1,5 +1,9 @@
 import numpy as np
 import itertools as it
+from collections import Counter
+from pathlib import Path
+
+rotation_matrices = [np.array([[int(c) for c in l.split()] for l in m.splitlines()]) for m in Path("rotation-matrices.txt").read_text().split("\n\n")]
 
 class NoCalibration(ValueError):
     pass
@@ -12,16 +16,16 @@ class Scanner:
     @property
     def rotations(self):
         for matrix in rotation_matrices:
-            yield Scanner(matrix@self.beacons)
+            yield Scanner((matrix@self.beacons.T).T)
 
     def calibrate_to(self, other):
-        # find overlapping beacons
-        # not sure how there's only one offset with at least 12 overlaps
-        # but I guess there must be somehow?
-        possible_offsets = ...
-        for offset in possible_offsets:
-            overlap = ...
-            if len(overlap) >= 12:
+        # assume we overlap completely and go through each possible map of corresponding beacons. 
+        # If we find at least 12 corresponding pairs are the same distance away from each other,
+        # then that must be the true offset
+        for order in it.permutations(range(len(self.beacons))):
+            mode, freq = Counter(map(tuple, other.beacons - self.beacons[order, :])).most_common()[0]
+            if freq >= 12:
+                offset = mode
                 break
         else:
             raise NoCalibration
@@ -31,14 +35,14 @@ class Scanner:
 
     @property
     def beaconset(self):
-        return {tuple(b) for b in self.beacons.T}
+        return {tuple(b) for b in self.beacons}
     
 def _parse(rawdata):
     scanners = []
     for scannerinfo in rawdata.split("\n\n"):
         beaconinfo = scannerinfo.splitlines()[1:]
         beacons = np.array([[int(n) for n in b.split(",")] for b in beaconinfo])
-        scanners.append(Scanner(beacons.T))
+        scanners.append(Scanner(beacons))
 
     return scanners
 
@@ -93,11 +97,11 @@ def part_1(*scanners):
         for s, u in it.product(solved,unsolved):
             for ur in u.rotations:
                 try:
-                    ur.calibate(s)
+                    ur.calibrate_to(s)
                 except NoCalibration:
                     continue 
                 else:
-                    solved.append(u)
+                    solved.append(ur)
                     unsolved.remove(u)
                     break
             else:
